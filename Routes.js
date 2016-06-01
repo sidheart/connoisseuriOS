81ccd14164cb326d550dcfcc2a3122712bd08209
Ty Giacalone <tygiacalone@gmail.com>
2016-06-01 15:21:26 -0700

    Change Routes.js back to use AWS

--- Routes.js
+++ Routes.js
@@ -2,7 +2,7 @@
  * Created by tygiacalone on 5/13/16.
  */
 
-const use_ec2 = false;
+const use_ec2 = true;
 var localhost = 'http://localhost:3000/';
 var ec2 = 'http://54.187.107.93:3000/';
 var url = use_ec2 ? ec2 : localhost;
