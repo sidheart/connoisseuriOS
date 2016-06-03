2a5aa7f6a791fe7581823b9e7a6dd4cccccae53f
Santo Chen <santo.1024@gmail.com>
2016-06-02 23:42:04 -0700

    CSS reorganization

--- CSS.js
+++ CSS.js
@@ -157,57 +157,9 @@ var css = StyleSheet.create({
       marginRight: width*0.15,
       marginTop: height* 0.03
     },
-    oneHalfWidth: {
-        width: width*0.5
-    },
-    oneHalf: {
-        height: containerHeight*0.5
-    },
-    oneThird: {
-        height: containerHeight*0.3333
-    },
-    oneFourth: {
-        height: containerHeight*0.25
-    },
-    oneSixth: {
-        height: containerHeight*0.1666
-    },
-    oneTenth: {
-        height: containerHeight*0.10
-    },
-    one11: {
-        height: containerHeight/11
-    },
-    one13: {
-        height: containerHeight/13
-    },
-    twoThirds: {
-        height: containerHeight*0.6667
-    },
-    twoFifths: {
-        height: containerHeight*0.4
-    },
-    threeFifths: {
-        height: containerHeight*0.6
-    },
-    fourFifths: {
-        height: containerHeight*0.8
-    },
     center: {
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    xspad: {
-        padding: 2
-    },
-    spad: {
-        padding: 10
-    },
-    mpad: {
-        padding: 20
-    },
-    lpad: {
-        padding: 30
     },
     hCenter: {
         alignItems: 'center'
@@ -217,45 +169,6 @@ var css = StyleSheet.create({
         backgroundColor: COLOR_WHITE,
         width: width*0.7
     },
-    h1: {
-        fontSize: 34
-    },
-    h2: {
-        fontSize: 24
-    },
-    h3: {
-        fontSize: 20
-    },
-    h4: {
-        fontSize: 16
-    },
-    skyblue: {
-        color: '#48BBEC'
-    },
-    bkSkyblue: {
-        backgroundColor: '#48BBEC'
-    },
-    black: {
-        color: '#000000'
-    },
-    bkBlack: {
-        backgroundColor: '#000000'
-    },
-    white: {
-        color: '#FFFFFF'
-    },
-    bkWhite: {
-        backgroundColor: '#FFFFFF'
-    },
-    gray: {
-        color: '#656565'
-    },
-    bkGray: {
-        backgroundColor: '#656565'
-    },
-    bold: {
-        fontWeight: 'bold'
-    }
 });
 
 module.exports = css;
