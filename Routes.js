cb09493682733094c72c35312efecb5dc438fea2
yingjia987 <yingjia987@users.noreply.github.com>
2016-06-09 13:54:15 -0700

    Updated new AWS URL

--- Routes.js
+++ Routes.js
@@ -4,7 +4,7 @@
 
 const use_ec2 = true;
 var localhost = 'http://localhost:3000/';
-var ec2 = 'http://54.187.107.93:3000/';
+var ec2 = 'http://52.11.254.37:3000';
 var url = use_ec2 ? ec2 : localhost;
 
 var Routes = {
