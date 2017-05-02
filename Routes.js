f0413b75ad8e74ec5a75ed8c2f78003b04b0b0e9
Sid Narayan <milk@Sids-Mac.local>
2017-05-02 16:54:34 -0700

    Added new background images and a logo image

--- Routes.js
+++ Routes.js
@@ -2,10 +2,10 @@
  * Created by tygiacalone on 5/13/16.
  */
 
-const use_ec2 = false;
+const use_ec2 = true;
 var localhost = 'http://localhost:3000/';
 var ec2 = 'http://52.11.254.37:3000/';
-var url = use_ec2 ? ec2 : localhost;
+var url = localhost;
 
 var Routes = {
   addUser: url + 'addUser',
